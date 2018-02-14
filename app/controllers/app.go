1038754ca7e13a425feb9d96edebc64e6b37c0f6
Khomkrid Lerdprasert <khomkrid@twinsynergy.co.th>
2018-02-14 16:52:55 +0700

    login view with bootstrap

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -11,3 +11,7 @@ type App struct {
 func (c App) Index() revel.Result {
 	return c.Render()
 }
+
+func (c App) Login(username string, password string) revel.Result {
+	return c.Render(username, password)
+}
